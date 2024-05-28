86de31f375d7a1768173143484fbe6952c95fe9e
Shreyas <shreyas.rentgpt@gmail.com>
2024-05-28 12:42:28 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-4xl text-blue-500 font-semibold tracking-tight'>Shadows Playground</h1>
+    </div>
+  )
+}
+
+export default App
