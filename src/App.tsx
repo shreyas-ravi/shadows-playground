a58182f57b7aae84024f8fe516382fd645b043a8
Shreyas <shreyas.rentgpt@gmail.com>
2024-05-28 21:00:42 -0400

    Nav and Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,148 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div id='wrapper'>
-      <div id='navbar'>Navbar</div>
-      <div id='content'>
-        <div id='sidebar'>Sidebar</div>
-        <div id='main'>
-          <div id='dashboard-content'>Dashboard Content</div>
-          <div id='shadows-panel'>Shadows Panel</div>
+    <div id="wrapper" className="bg-zinc-100">
+      <div id="navbar" className="bg-zinc-100">
+        <div id="navbar-container">
+          <div id="profile-container">
+            <a className="bg-zinc-500 text-white w-6 h-6 flex items-center justify-center rounded-full">
+              S
+            </a>
+            <a>
+              Shreyas <span className="text-slate-500">/</span> Shadows Console
+            </a>
+          </div>
+          <div id="link-container">
+            <a className="text-slate-900 font-semibold cursor-pointer">
+              Dashboard
+            </a>
+            <a className="text-slate-600 cursor-pointer hover:text-slate-900">
+              Docs
+            </a>
+            <a className="text-slate-600 cursor-pointer hover:text-slate-900">
+              API Reference
+            </a>
+            <a>
+              <svg
+                className="w-6 h-6 text-gray-500 dark:text-white cursor-pointer hover:text-gray-600"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M17 10v1.126c.367.095.714.24 1.032.428l.796-.797 1.415 1.415-.797.796c.188.318.333.665.428 1.032H21v2h-1.126c-.095.367-.24.714-.428 1.032l.797.796-1.415 1.415-.796-.797a3.979 3.979 0 0 1-1.032.428V20h-2v-1.126a3.977 3.977 0 0 1-1.032-.428l-.796.797-1.415-1.415.797-.796A3.975 3.975 0 0 1 12.126 16H11v-2h1.126c.095-.367.24-.714.428-1.032l-.797-.796 1.415-1.415.796.797A3.977 3.977 0 0 1 15 11.126V10h2Zm.406 3.578.016.016c.354.358.574.85.578 1.392v.028a2 2 0 0 1-3.409 1.406l-.01-.012a2 2 0 0 1 2.826-2.83ZM5 8a4 4 0 1 1 7.938.703 7.029 7.029 0 0 0-3.235 3.235A4 4 0 0 1 5 8Zm4.29 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h6.101A6.979 6.979 0 0 1 9 15c0-.695.101-1.366.29-2Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div id="content">
+        <div id="sidebar" className="bg-zinc-50">
+          <a
+            href="#"
+            className="mt-2 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group"
+          >
+            <svg
+              className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 18"
+            >
+              <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+            </svg>
+            <span className="flex-1 ms-3 whitespace-nowrap">Your Shadows</span>
+          </a>
+          <ul className="ml-11">
+            <li className="text-gray-600 dark:text-white cursor-pointer hover:bg-gray-200">
+              Shreyas
+            </li>
+            <li className="text-gray-600 dark:text-white cursor-pointer hover:bg-gray-200">
+              Himanshu
+            </li>
+            <li className="text-gray-600 dark:text-white cursor-pointer hover:bg-gray-200">
+              Reshma
+            </li>
+            <li className="text-gray-600 dark:text-white cursor-pointer hover:bg-gray-200">
+              Ashesh
+            </li>
+            <li className="text-gray-600 dark:text-white cursor-pointer hover:bg-gray-200">
+              Soumojit
+            </li>
+          </ul>
+          <a
+            href="#"
+            className="mt-4 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group"
+          >
+            <svg
+              className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+              <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+              <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+            </svg>
+            <span className="flex-1 ms-3 whitespace-nowrap">Documents</span>
+          </a>
+          <a
+            href="#"
+            className="mt-2 px-1 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group"
+          >
+            <svg
+              className="w-6 h-6 text-gray-500 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+
+            <span className="flex-1 ms-3 whitespace-nowrap">Usage</span>
+          </a>
+
+          <a
+            href="#"
+            className="mt-2 px-1 flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700 group"
+          >
+            <svg
+              className="w-6 h-6 text-gray-500 dark:text-white cursor-pointer hover:text-gray-600"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 7.205c4.418 0 8-1.165 8-2.602C20 3.165 16.418 2 12 2S4 3.165 4 4.603c0 1.437 3.582 2.602 8 2.602ZM12 22c4.963 0 8-1.686 8-2.603v-4.404c-.052.032-.112.06-.165.09a7.75 7.75 0 0 1-.745.387c-.193.088-.394.173-.6.253-.063.024-.124.05-.189.073a18.934 18.934 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.073a10.143 10.143 0 0 1-.852-.373 7.75 7.75 0 0 1-.493-.267c-.053-.03-.113-.058-.165-.09v4.404C4 20.315 7.037 22 12 22Zm7.09-13.928a9.91 9.91 0 0 1-.6.253c-.063.025-.124.05-.189.074a18.935 18.935 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.074a10.163 10.163 0 0 1-.852-.372 7.816 7.816 0 0 1-.493-.268c-.055-.03-.115-.058-.167-.09V12c0 .917 3.037 2.603 8 2.603s8-1.686 8-2.603V7.596c-.052.031-.112.059-.165.09a7.816 7.816 0 0 1-.745.386Z" />
+            </svg>
+
+            <span className="flex-1 ms-3 whitespace-nowrap">Storage</span>
+          </a>
+        </div>
+        <div id="main" className="bg-white">
+          <div id="dashboard-content">Dashboard Content</div>
+          <div id="shadows-panel">Shadows Panel</div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
